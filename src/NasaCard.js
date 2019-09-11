c5d51be2ef651e1e3b328cbd5b562750982c7eed
Nicole Hollis <nicole@emmabeauty.co>
2019-09-11 16:37:38 -0700

    MVP in progress

---
+++ src/NasaCard.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "../node_modules/axios"
+
+cfunction NasaLists() {
+  const [nasa, setNasa] = useState([]);
+  const [day, setDate] = useState("1");
+  useEffect(() => {
+    axios
+      .get(
+        (`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-08-${day}`).then(res => {
+        console.log(res.data);
+        const nasa = res.data;
+        setNasa(nasa);
+        console.log(setDate());
+      })
+      .catch(err => {
+        console.log("The Data was not returned", err);
+      });
+  }, [day]);
+  return (
+    <div>
+      <NasaCards
+        key={nasa.id}
+        title={nasa.title}
+        explanation={nasa.explanation}
+        imgUrl={nasa.url}
+        date={nasa.date}
+        copyright={nasa.copyright}
+      />
+      <div>
+        <button
+          onClick={() =>
+            setDate(() => {
+              Math.floor(Math.random() * 30);
+            })
+          }
+        >
+          Click For Random
+        </button>
+      </div>
+    </div>
+  );
+}
+export default NasaLists;
+`
